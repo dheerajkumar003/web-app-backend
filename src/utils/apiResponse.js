d80a3c466ff8dd59026414e184cb2dcfe58b7615
RNDevDheeraj <dheeraj616521@gmail.com>
2024-06-15 12:21:38 +0530

    added user and video model

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -2,7 +2,7 @@ class ApiResponse {
   constructor(statusCode, data, message = "Success") {
     this.statusCode = statusCode;
     this.data = data;
-    this.statusCode.message = message;
+    this.message = message;
     this.success = success < 400;
   }
 }
