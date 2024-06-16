ac7c326913cf064c642a96e6e059d33d574d13f7
RNDevDheeraj <dheeraj616521@gmail.com>
2024-06-17 01:25:55 +0530

    all configuratins done, created route

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,12 @@
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
       next(error)
     );
   };
 };
+
+export { asyncHandler };
 
 // const asyncHandler = ()=>{}
 // const asyncHandler = (func)=> ()=>{}
